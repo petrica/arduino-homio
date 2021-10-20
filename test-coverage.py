8542b25611afd4888873361960a5927a2bc7f070
Petrica Martinescu <petrica.martinescu@gmail.com>
2021-10-20 22:48:31 +0300

    Add github workflow

--- test-coverage.py
+++ test-coverage.py
@@ -8,11 +8,12 @@ env.Append(
 )
 
 def generateCoverageInfo(source, target, env):
-    for file in os.listdir("test"):
-        os.system(".pio/build/test_device/program test/"+file)
+    # for file in os.listdir("test"):
+    #     os.system(".pio/build/test_device/program test/"+file)
     os.system("lcov -d .pio/build/test_device/src -c -o lcov.info")
-    os.system("lcov --remove lcov.info '*.pio*' '*v1*' -o filtered_lcov.info")
-    os.system("genhtml -o coverage/ filtered_lcov.info")
-    os.system("rm lcov.info filtered_lcov.info")
+    os.system("lcov --extract lcov.info '*src*' -o extract_lcov.info")
+    os.system("lcov --remove extract_lcov.info '*.pio*' '*homio.h' '*main.cpp' -o lcov.info")
+    os.system("genhtml -o coverage/ lcov.info")
+    os.system("rm extract_lcov.info")
 
 env.AddPostAction(".pio/build/test_device/program", generateCoverageInfo)
