2bcd19485b41a69d70058d61618b3e08332b7f05
Petrica Martinescu <petrica.martinescu@gmail.com>
2021-10-20 22:55:42 +0300

    Execute program for test coverage

--- test-coverage.py
+++ test-coverage.py
@@ -8,8 +8,8 @@ env.Append(
 )
 
 def generateCoverageInfo(source, target, env):
-    # for file in os.listdir("test"):
-    #     os.system(".pio/build/test_device/program test/"+file)
+    for file in os.listdir("test"):
+        os.system(".pio/build/test_device/program test/"+file)
     os.system("lcov -d .pio/build/test_device/src -c -o lcov.info")
     os.system("lcov --extract lcov.info '*src*' -o extract_lcov.info")
     os.system("lcov --remove extract_lcov.info '*.pio*' '*homio.h' '*main.cpp' -o lcov.info")
